5033c1ce6def83dfda0b887f1fb4fc24e3b78947
reactive_cat <mykola.papenko@gmail.com>
2020-08-22 20:24:31 +0300

    try to make inline keyboard

--- src/main/main.go
+++ src/main/main.go
@@ -46,24 +46,34 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 // Create a struct to conform to the JSON body
 // of the send message request
 // https://core.telegram.org/bots/api#sendmessage
-type sendMessageReqBody struct {
-	ChatID int64  `json:"chat_id"`
-	Text   string `json:"text"`
+type SendMessageReqBody struct {
+	ChatID      int64                `json:"chat_id"`
+	Text        string               `json:"text"`
+	ReplyMarkup InlineKeyboardMarkup `json:"reply_markup"`
 }
 
 // startGame takes a chatID and sends "polo" to them
 func startGame(chatID int64) error {
+
+	keys := [3]string{"Name", "Language", "API"}
+	values := [3]string{"John", "Python", "pyTelegramBotAPI"}
+	markup := InlineKeyboardMarkup{}
+	for i := 0; i < 3; i++ {
+		markup.InlineKeyboard[i][0] = InlineKeyboardButton{Text: keys[i]}
+		markup.InlineKeyboard[i][1] = InlineKeyboardButton{Text: values[i]}
+	}
+
 	// Create the request body struct
-	reqBody := &sendMessageReqBody{
-		ChatID: chatID,
-		Text:   "Polo!!",
+	reqBody := &SendMessageReqBody{
+		ChatID:      chatID,
+		Text:        "Polo!!",
+		ReplyMarkup: markup,
 	}
 	// Create the JSON body from the struct
 	reqBytes, err := json.Marshal(reqBody)
 	if err != nil {
 		return err
 	}
-
 	// Send a post request with your token
 	res, err := http.Post("https://api.telegram.org/bot"+os.Getenv("BOT_TOKEN")+"/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
 
